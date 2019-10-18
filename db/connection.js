de88089ab87cab94b693aabc3f3f80d6d3a1436f
James Opara <jamesopara@Jamess-MacBook-Pro.local>
2019-10-17 22:32:12 -0500

    Deploying to Heroku in progress

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 
-//Mongoose Setup
-mongoose.connect("mongodb://localhost/hacksDB", { useNewUrlParser: true });
+
 mongoose.Promise = global.Promise;
+
+//Mongoose Setup
+
+let mongoURI = "";
+if (process.env.NODE_ENV === 'production') {
+	mongoURI = process.env.DB_URL;
+} else {
+	mongoURI = 'mongodb://localhost/hacksDB';
+}
+
+
+mongoose.connect(mongoURI, { useNewUrlParser: true });
+
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const hacks = require("./controllers/hacks");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
-//middleware
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "hbs");
 app.use(methodOverride("_method"));
 app.use("/assets", express.static("public"));
-
-//local host
-app.listen(5000, () => console.log("connected on 5000"));
 app.use("/", hacks);
+
+// Deployment
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
